221f5c9b3b7bf1f073fa0bd54e478dc0d2e014d3
Acutapugione <acuta.pugione@gmail.com>
2022-06-10 10:56:07 +0300

    MigrationUpdate(Заявка+Потребители)

---
+++ Operativka/Migrations/20220610075452_Заявка+Потребители.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Operativka.Migrations
+{
+    public partial class ЗаявкаПотребители : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Consumers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Patronymic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PersonalAccountCode = table.Column<int>(type: "int", nullable: true),
+                    DistrictId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consumers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consumers_Districts_DistrictId",
+                        column: x => x.DistrictId,
+                        principalTable: "Districts",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationDocuments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConsumerId = table.Column<int>(type: "int", nullable: false),
+                    OuterAppNum = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsFromOuterApp = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationDocuments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApplicationDocuments_Consumers_ConsumerId",
+                        column: x => x.ConsumerId,
+                        principalTable: "Consumers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationObjectives",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(type: "int", nullable: false),
+                    PlannedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ExecutionDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    IsExecuted = table.Column<bool>(type: "bit", nullable: false),
+                    ApplicationDocumentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationObjectives", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ApplicationObjectives_ApplicationDocuments_ApplicationDocumentId",
+                        column: x => x.ApplicationDocumentId,
+                        principalTable: "ApplicationDocuments",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationDocuments_ConsumerId",
+                table: "ApplicationDocuments",
+                column: "ConsumerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationObjectives_ApplicationDocumentId",
+                table: "ApplicationObjectives",
+                column: "ApplicationDocumentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consumers_DistrictId",
+                table: "Consumers",
+                column: "DistrictId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationObjectives");
+
+            migrationBuilder.DropTable(
+                name: "ApplicationDocuments");
+
+            migrationBuilder.DropTable(
+                name: "Consumers");
+        }
+    }
+}
